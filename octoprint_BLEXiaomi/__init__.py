39d9ea9dc77c7387431894f6e5aff0ebc4b41e36
Rusky <informatic5.0@gmail.com>
2021-09-12 01:16:17 +0100

    Naming Schema Fix

--- octoprint_BLEXiaomi/__init__.py
+++ octoprint_BLEXiaomi/__init__.py
@@ -21,38 +21,38 @@ class XiaomiBLE(octoprint.plugin.StartupPlugin,
     def get_template_vars(self):
         return dict(url=self._settings.get(["url"]))
 
-    def test():
-    # Use 0 for hci0
-    dev_id = 0
-    toggle_device(dev_id, True)
+    def test(self):
+        # Use 0 for hci0
+        dev_id = 0
+        toggle_device(dev_id, True)
 
-    try:
-        sock = bluez.hci_open_dev(dev_id)
-    except:
-        print("Cannot open bluetooth device %i" % dev_id)
-        raise
+        try:
+            sock = bluez.hci_open_dev(dev_id)
+        except:
+            print("Cannot open bluetooth device %i" % dev_id)
+            raise
 
-    # Set filter to "True" to see only one packet per device
-    enable_le_scan(sock, filter_duplicates=False)
+        # Set filter to "True" to see only one packet per device
+        enable_le_scan(sock, filter_duplicates=False)
 
-    try:
-        def le_advertise_packet_handler(mac, adv_type, data, rssi):
-            data_str = raw_packet_to_str(data)
-            # Check for ATC preamble
-            if data_str[5:10] == '61a18':
-                temp = int(data_str[22:26], 16) / 10
-                hum = int(data_str[26:28], 16)
-                batt = int(data_str[28:30], 16)
-                print("%s - Device: %s Temp: %sc Humidity: %s%% Batt: %s%%" % \
-                     (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mac, temp, hum, batt))
+        try:
+            def le_advertise_packet_handler(mac, adv_type, data, rssi):
+                data_str = raw_packet_to_str(data)
+                # Check for ATC preamble
+                if data_str[5:10] == '61a18':
+                    temp = int(data_str[22:26], 16) / 10
+                    hum = int(data_str[26:28], 16)
+                    batt = int(data_str[28:30], 16)
+                    print("%s - Device: %s Temp: %sc Humidity: %s%% Batt: %s%%" % \
+                         (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mac, temp, hum, batt))
 
-        # Called on new LE packet
-        parse_le_advertising_events(sock,
-                                    handler=le_advertise_packet_handler,
-                                    debug=False)
-    # Scan until Ctrl-C
-    except KeyboardInterrupt:
-        disable_le_scan(sock)
+            # Called on new LE packet
+            parse_le_advertising_events(sock,
+                                        handler=le_advertise_packet_handler,
+                                        debug=False)
+        # Scan until Ctrl-C
+        except KeyboardInterrupt:
+            disable_le_scan(sock)
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = XiaomiBLE()

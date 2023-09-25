a6f40a8dec1f760c119a9d3e8d052d2874915593
Rusky <informatic5.0@gmail.com>
2023-09-25 21:32:17 +0100

    More changes.

--- XiaomiBLEThermo/__init__.py
+++ XiaomiBLEThermo/__init__.py
@@ -20,7 +20,6 @@ class XiaomiBLEThermo(octoprint.plugin.StartupPlugin,
         self.dev_id = 0
 
     def on_after_startup(self):
-        self._logger.info("XiaomiBLEThermo plugin started!")
         self._logger.info("\033[94m[Bluetooth]\033[0m XiaomiBLEThermo plugin started!")
 
     def get_settings_defaults(self):
@@ -40,12 +39,13 @@ class XiaomiBLEThermo(octoprint.plugin.StartupPlugin,
         )
 
     def start_scan(self):
+        self._logger.info("Start scan method called")
         toggle_device(self.dev_id, True)
-
+        self._logger.info("\033[94m Scan Start !\033[0m")    
         try:
             sock = bluez.hci_open_dev(self.dev_id)
         except:
-            self._logger.error("Cannot open bluetooth device %i" % self.dev_id)
+            self._logger.error("Cannot open \033[94m bluetooth \033[0m device %i" % self.dev_id)
             raise
 
         # Set filter to "True" to see only one packet per device
@@ -65,16 +65,14 @@ class XiaomiBLEThermo(octoprint.plugin.StartupPlugin,
                     batt = int(data_str[28:30], 16)
                     self.latest_ble_data = {
                         "temperature": temp,
-                        "humidity": hum,
-                        "battery": batt
+                        "humidity": hum
                     }
                     self._logger.info(
-                        "%s - Device: %s Temp: %sc Humidity: %s%% Batt: %s%%" % (
+                        "%s - Device: %s Temp: %sc Humidity: %s%%" % (
                             datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                             mac,
                             temp,
                             hum,
-                            batt,
                         )
                     )
                 else:
@@ -100,7 +98,12 @@ class XiaomiBLEThermo(octoprint.plugin.StartupPlugin,
         if self.latest_ble_data:
             return flask.jsonify(self.latest_ble_data)
         else:
-            return flask.jsonify(error="No BLE data available")
+            return flask.jsonify({
+                "temperature": "N/A",
+                "humidity": "N/A"
+            })
+
+
 
 __plugin_pythoncompat__ = ">=3.8,<4"
 __plugin_name__ = "XiaomiBLEThermo"
